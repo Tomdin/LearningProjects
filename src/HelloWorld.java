ab8a690038ab04237e37f11f2fb57710c261b316
会飞的年糕 <212300263@qq.com>
2022-10-14 15:20:20 +0800

    Hello World!

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    /**
+     * Hello World!
+     * @param args
+     */
+    public static void main(String[] args) {
+        //TODO 第一个Java程序!
+        System.out.println("Hello World!");
+    }
+}
